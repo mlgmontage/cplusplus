109d46f2a9426e255bff0fe1019011fe19404b4f
mlgmontage <kettleofrassel1@gmail.com>
2020-05-10 04:06:45 -0700

    reference

--- main.cpp
+++ main.cpp
@@ -2,6 +2,12 @@
 
 using namespace std;
 
+// reference
+void Increment(int *value)
+{
+   (*value)++;
+}
+
 int main()
 {
    int var = 8;
@@ -9,6 +15,14 @@ int main()
 
    *var_ptr = 12;
 
+   // reference
+
+   int a = 5;
+   int &ref = a;
+
    cout << var_ptr << " -> " << *var_ptr << endl;
+   cout << "---------------------" << endl;
+   Increment(&a);
+   cout << ref << endl;
    return 0;
 }
