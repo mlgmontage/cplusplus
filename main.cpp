dc821ea00e31d1983d7b5dbb3762e40d0eeb9edd
mlgmontage <kettleofrassel1@gmail.com>
2020-05-10 03:01:17 -0700

    pointers

--- main.cpp
+++ main.cpp
@@ -2,31 +2,13 @@
 
 using namespace std;
 
-int sum(int a, int b)
-{
-   return a + b;
-}
-
-int fact(int n)
-{
-   int product = 1;
-   for (int i = 1; i <= n; i++)
-   {
-      product *= i;
-   }
-
-   return product;
-}
-
 int main()
 {
-   int finish = 10;
-   for (int i = 0; i < finish; i++)
-   {
-      cout << "factorial of " << i << " -> " << fact(i) << endl;
-   }
+   int var = 8;
+   int *var_ptr = &var;
 
-   cout << "sum of 3 and 5 -> " << sum(3, 5) << endl;
+   *var_ptr = 12;
 
+   cout << var_ptr << " -> " << *var_ptr << endl;
    return 0;
 }
